f9fbabf009ad4ae7d95d2411c7843e86d0de2248
AndreyPimkin <pimkin03@bk.ru>
2021-11-16 19:39:56 +0300

    Сделал первое активити более работоспособной

--- app/src/main/java/com/example/top/MainActivity.java
+++ app/src/main/java/com/example/top/MainActivity.java
@@ -4,27 +4,42 @@ package com.example.top;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    // набор данных, которые свяжем со списком
-    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // получаем элемент ListView
-        ListView countriesList = (ListView) findViewById(R.id.countriesList);
-
+        /*получаем элемент TextView
+        TextView selection = findViewById(R.id.view); */
+        ListView listTeams = findViewById(R.id.topTeamList);
+        // получаем ресурс
+        String[] listTeamResource = getResources().getStringArray(R.array.listTeam);
         // создаем адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, countries);
-
-        // устанавливаем для списка адаптер
-        countriesList.setAdapter(adapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listTeamResource);
+        listTeams.setAdapter(adapter);
+        listTeams.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                // по позиции получаем выбранный элемент
+                String selectedItem = listTeamResource[position];
+                Intent intentinfo = new Intent(MainActivity.this, SecondActivity.class);
+                intentinfo.putExtra("choice", selectedItem);
+                startActivity(intentinfo);
+            }
+        });
     }
+
+
+
 }
